599889cda57af631cd3ccdceaf5a8c1c0c188ad7
Chengwei Wang <chengwei.wendy.wang@gmail.com>
2020-11-13 21:58:17 -0500

    add tests

--- cipher_cw3210/tests/test_cipher_cw3210.py
+++ cipher_cw3210/tests/test_cipher_cw3210.py
@@ -1,5 +1,10 @@
-from cipher_cw3210 import __version__
-from cipher_cw3210 import cipher_cw3210
+from cipher_cw3210.cipher_cw3210 import cipher
 
-def test_version():
-    assert __version__ == 0.1.0
+def test_cipher_symbols():
+    example = 'Yellow!'
+    shift = 1
+    expected = 'Zfmmpx!'
+    actual = cipher(example, shift, encrypt=True)
+    assert actual == expected
+
+test_cipher_symbols()
